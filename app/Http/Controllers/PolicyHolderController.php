24c8427f5b84348b636a967b9583912d9176fb6a
mashood_ali <mashood.ali@algorepublic.com>
2020-10-15 11:48:15 +0500

    payment change

--- app/Http/Controllers/PolicyHolderController.php
+++ app/Http/Controllers/PolicyHolderController.php
@@ -547,8 +547,10 @@ class PolicyHolderController extends Controller
         //$cartTotal = 10.00;// This amount needs to be sourced from your application
         $data = array(
             // Merchant details
-            'merchant_id' => '10012141',
-            'merchant_key' => '7goueleoh3b0m',
+            'merchant_id' => '16311179',
+            'merchant_key' => 'moxa3jyzm5ubx',
+            //'merchant_id' => '10012141', // test
+            //'merchant_key' => '7goueleoh3b0m', // test
             'return_url' => $baseUrl . '/payfast-success',
             'cancel_url' => $baseUrl . '/payfast-cancel',
             'notify_url' => $baseUrl . '/payfast-notify',
@@ -563,18 +565,19 @@ class PolicyHolderController extends Controller
             'custom_int1' => (int) $userID,
             'custom_int2' => (int) $packageID,
             'custom_str1' => $period,
-            //'payment_method' => 'eft',
+            'payment_method' => 'eft',
             'subscription_type' => 1,
             'billing_date' => date('Y-m-d'),
             'frequency' => (int) $frequency,
             'cycles' => 0
         );
 
-        $signature = $this->generateSignature($data, 'Testpassphrase123');
+        //$signature = $this->generateSignature($data, 'Testpassphrase123');
+        $signature = $this->generateSignature($data);
         $data['signature'] = $signature;
 
         // If in testing mode make use of either sandbox.payfast.co.za or www.payfast.co.za
-        $testingMode = true;
+        $testingMode = false;
         $pfHost = $testingMode ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
         $htmlForm = '<form id="myForm" action="https://'.$pfHost.'/eng/process" method="post">';
         foreach($data as $name=> $value)
