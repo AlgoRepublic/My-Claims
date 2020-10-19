79413d6071e1906d12e70bed0860a4df31cfee8c
mashood_ali <mashood.ali@algorepublic.com>
2020-10-19 16:38:48 +0500

    Confirm message change

--- resources/views/beneficiary/home.blade.php
+++ resources/views/beneficiary/home.blade.php
@@ -12,27 +12,27 @@
                     {{ Session::get('message') }}
                 </div>
             @endif
-            <form action="{{ url('/beneficiary/find-policy') }}" method="post">
-                @csrf
-                <div class="custom_form_heading"><span>To see what policies your loved-ones have left you as a beneficiary, enter the policyholder’s identity number.</span></div>
-                <div class="form-row">
-                    <div class="form-group col-md-12">
-                        <label for=" ">Enter Policyholder’s Identity Number<span class="error-text">*</span></label>
-                        <input type="text" class="form-control" name="policyholder_number" placeholder="5453453643" required>
-                    </div>
+            @if(Session::has('message') && Session::get('alert-class') == 'alert-success')
+                <div class="form-group text-center">
+                    <a class="btn btn-lg custom_btn_delete" href="{{ url('/') }}">Home</a>
                 </div>
-                {{--<div class="form-row">
-                    <div class="form-group col-md-12">
-                        <label for=" ">Enter Beneficiary's Identity Number<span class="error-text">*</span></label>
-                        <input type="text" class="form-control" name="beneficiary_number" placeholder="5453453643" required>
+            @else
+                <form action="{{ url('/beneficiary/find-policy') }}" method="post">
+                    @csrf
+                    <div class="custom_form_heading"><span>To see what policies your loved-ones have left you as a beneficiary, enter the policyholder’s identity number.</span></div>
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <label for=" ">Enter Policyholder’s Identity Number<span class="error-text">*</span></label>
+                            <input type="text" class="form-control" name="policyholder_number" placeholder="5453453643" required>
+                        </div>
                     </div>
-                </div>--}}
-                <div class="form-row justify-content-center">
-                    <div class="form-group">
-                        <input type="submit" class="btn custom_btn_form" value="Check">
+                    <div class="form-row justify-content-center">
+                        <div class="form-group">
+                            <input type="submit" class="btn custom_btn_form" value="Check">
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            @endif
         </div>
     </div>
 @endsection
